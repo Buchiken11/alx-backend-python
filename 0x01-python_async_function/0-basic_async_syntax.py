188a29ef2885159aa0cc17c1b32b34720f4efbf4
Buchiken11 <buchiken025@gmail.com>
2024-08-06 18:22:23 +0100

    basic asyncio

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import asyncio
+import random
+
+async def wait_random(max_delay=10):
+    duration = random.uniform(0, max_delay)
+    await asyncio.sleep(duration)
+    return duration
+
+if __name__ == "__main__":
+    asyncio.run(awit_random())
