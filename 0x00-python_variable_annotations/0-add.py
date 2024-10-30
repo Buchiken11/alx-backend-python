0d81a0f1da76208ac6251d1cf5d36519cda14549
Buchiken11 <buchiken025@gmail.com>
2024-10-30 12:04:51 +0100

    updating all files

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3 env
+
+""" typescript """
+def add(a: float, b: float) -> float:
+    """
+    Args:
+        a: float argument
+        b: float argument
+
+    Returns: the sum of a and b as float
+    """
+    return (a + b)
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""
+Type-annotated function concat that concatenates strings
+"""
+
+def concat(str1: str, str2: str) -> str:
+    """Takes two str variables and returns their concatenation"""
+    return str1 + str2
